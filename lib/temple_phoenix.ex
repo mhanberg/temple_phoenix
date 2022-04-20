e220d7ebea18ac676d038fef5d7a3025e6ad2e73
Mitchell Hanberg <mitch@mitchellhanberg.com>
2022-04-19 23:04:32 -0400

    Initial Commit

---
+++ lib/temple_phoenix.ex
@@ -0,0 +1,18 @@
+defmodule TemplePhoenix do
+  @moduledoc """
+  Documentation for `TemplePhoenix`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TemplePhoenix.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/temple_phoenix_test.exs
@@ -0,0 +1,8 @@
+defmodule TemplePhoenixTest do
+  use ExUnit.Case
+  doctest TemplePhoenix
+
+  test "greets the world" do
+    assert TemplePhoenix.hello() == :world
+  end
+end
